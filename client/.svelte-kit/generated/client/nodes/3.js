2007b2dbdc703f135a6287a71332eb3f88fe6dc1
D-Velopment <i.velopment@gmail.com>
2026-03-30 20:09:50 +0300

    Remove artefacts

--- client/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/chat/[roomHash]/+page.svelte";
--- client/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.B-2j2Bc4.js","_app/immutable/chunks/D39ZREKu.js","_app/immutable/chunks/7u6au6bF.js","_app/immutable/chunks/CvL5-glY.js","_app/immutable/chunks/PTdMS6Db.js","_app/immutable/chunks/Bu3m2SW9.js","_app/immutable/chunks/D5GcQ4cI.js","_app/immutable/chunks/CVs6drcE.js"];
-export const stylesheets = ["_app/immutable/assets/0.rQNqYRG8.css"];
-export const fonts = [];
